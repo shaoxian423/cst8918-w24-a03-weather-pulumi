b09e69b9307ea1e629549b8cdaf9366ed5b40fdb
shaoxian423 <shaoxianduan@DuanM4.local>
2025-05-22 18:52:43 -0400

    first 50 lines, Shaoxian Duan

---
+++ infrastructure/index.ts
@@ -0,0 +1,49 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from '@pulumi/azure-native/resources'
+import * as containerregistry from '@pulumi/azure-native/containerregistry'
+// Other imports at the top of the module
+import * as docker from '@pulumi/docker'
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
+
+
+// Import the configuration settings for the current stack.
+const config = new pulumi.Config()
+const appPath = config.get('appPath') || '../'
+const prefixName = config.get('prefixName') || 'cst8918-a03-student'
+const imageName = prefixName
+const imageTag = config.get('imageTag') || 'latest'
+// Azure container instances (ACI) service does not yet support port mapping
+// so, the containerPort and publicPort must be the same
+const containerPort = config.getNumber('containerPort') || 80
+const publicPort = config.getNumber('publicPort') || 80
+const cpu = config.getNumber('cpu') || 1
+const memory = config.getNumber('memory') || 2
+
+// Create a resource group.
+const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+
+// Create the container registry.
+const registry = new containerregistry.Registry(`${prefixName}ACR`, {
+  resourceGroupName: resourceGroup.name,
+  adminUserEnabled: true,
+  sku: {
+    name: containerregistry.SkuName.Basic
+  }
+})
+
+// Get the authentication credentials for the container registry.
+const registryCredentials = containerregistry
+  .listRegistryCredentialsOutput({
+    resourceGroupName: resourceGroup.name,
+    registryName: registry.name
+  })
+  .apply(creds => {
+    return {
+      username: creds.username!,
+      password: creds.passwords![0].value!
+    }
+  })
+
+export const acrServer = registry.loginServer
+export const acrUsername = registryCredentials.username
+
